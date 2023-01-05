742dba643296b102825cf5d6c03f4f6761c60951
Riacule <sophie.earl@live.com>
2023-01-05 17:36:04 +0000

    Initial commit

---
+++ src/routes/optionsBar.svelte
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<section>
+	<a href="/">Home</a>
+	<a href="/">Explore</a>
+	<a href="/">Notifications</a>
+	<a href="/">Messages</a>
+	<a href="/">Bookmarks</a>
+	<a href="/">Lists</a>
+	<a href="/">Profile</a>
+	<a href="/">More</a>
+
+	<button href="/">Tweet</button>
+</section>
+
+<style>
+	a {
+		display: flex;
+		flex-direction: column;
+
+		font-size: larger;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+
+		padding: 25px;
+		text-decoration: none;
+	}
+
+	button {
+		background-color: #1da1f2;
+		outline: #1da1f2;
+		cursor: pointer;
+		border: 2px solid transparent;
+		border-radius: 0.9rem;
+		width: 20%;
+		min-height: 2rem;
+		color: white;
+	}
+	button:focus {
+		box-shadow: none;
+		outline: 2px solid transparent;
+		outline-offset: 2px;
+	}
+</style>
