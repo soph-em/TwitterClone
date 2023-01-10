2391a142be8eb111e35b835f070b5a1ecf1d0917
Riacule <sophie.earl@live.com>
2023-01-10 02:39:03 +0000

    feed CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,6 @@
 	console.log(data);
 </script>
 
-<!-- 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 <section>
 	<div class="options">
 		<OptionsBar />
